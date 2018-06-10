9caba439227ae024f4c16dfb98cd14164e7879fa
David Hopkins <dlhopkins2@gmail.com>
2018-06-10 23:49:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,8 +25,8 @@ class Transfer
 
   def reverse_transfer
     if @status == "completed"
-      receiver.balance -= amount
-      sender.balance += amount
+      receiver.balance -= @amount
+      sender.balance += @amount
       @status = "reversed"
     else
       false
