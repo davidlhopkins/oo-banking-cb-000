8b8ffbc18a942aefefb6af55ee24c8162db43b8f
David Hopkins <dlhopkins2@gmail.com>
2018-06-10 23:49:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
     if valid? && sender.balance >= @amount && @status == 'pending'
       sender.balance -= @amount
       receiver.balance += @amount
-      @status = "completed"
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
