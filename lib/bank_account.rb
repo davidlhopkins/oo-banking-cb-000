f8083f680864b89421cfd883607f1c231d0ef0c5
David Hopkins <dlhopkins2@gmail.com>
2018-06-10 23:51:26 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,11 +17,11 @@ class BankAccount
   end
 
   def valid?
-    self.status == "open" && self.balance > 0 ? true : false
+    self.status == 'open' && self.balance > 0 ? true : false
   end
 
   def close_account
-    self.status = "closed"
+    self.status = 'closed'
   end
 
 end
