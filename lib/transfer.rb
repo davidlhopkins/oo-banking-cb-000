912d5e7db51bb0213ef3873ad35057db7a67b9d1
David Hopkins <dlhopkins2@gmail.com>
2018-06-10 23:43:32 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
     if sender.valid? & receiver.valid? == true & @status = "pending"
       sender.balance -= amount
       receiver.balance += amount
-      @status = "complete"
+      @status = "completed"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
@@ -24,6 +24,7 @@ class Transfer
   end
 
   def reverse_transfer
+    if @status = "completed"
     receiver -= amount
     sender += amount
   end
